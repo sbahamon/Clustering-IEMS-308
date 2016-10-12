b6d6d5400ea0399bb5755c5488332663f0f9b770
Steffany Bahamon <sbahamon1@gmail.com>
2016-10-12 14:54:53 -0500

    Update madicare_clustering.py

--- madicare_clustering.py
+++ madicare_clustering.py
@@ -4,39 +4,26 @@ Created on Wed Oct 12 11:21:01 2016
 
 @author: sbv092
 """
-
 import numpy as np
 from sklearn.cluster import KMeans
-from scipy.spatial.distance import cdist
+from sklearn.decomposition import PCA
+#from scipy.spatial import Voronoi, voronoi_plot_2d
 import matplotlib.pyplot as plt
-#from sklearn.decomposition import PCA
 
 data = np.loadtxt("clean_providers.csv", delimiter = ",")
 
-K = range(1,10)
-meandistortions = []
-for k in K:
-    km = KMeans(n_clusters = k)
-    km.fit(data)
-    meandistortions.append(sum(np.min(cdist(data, km.cluster_centers_, 'euclidean'), axis=1)) / data.shape[0])
-    
-plt.plot(K, meandistortions, 'bx-')
-plt.xlabel('k')
-plt.ylabel('Average Distortion')
-plt.title('Selecting k with the Elbow Method')
-plt.show()
-
 #cluster
-#km = KMeans(n_clusters = 4)
-#km.fit(data)
+km = KMeans(n_clusters = 6).fit(data)
+km.fit(data)
 
-"""
 reduced_data = PCA(n_components = 2).fit_transform(data)
-kmeans = KMeans(n_clusters = 4)
-kmeans.fit(reduced_data)
+r_km = KMeans(n_clusters = 6).fit(reduced_data)
 
-# Step size of the mesh. Decrease to increase the quality of the VQ.
-h = .02     # point in the mesh [x_min, x_max]x[y_min, y_max].
+#vor = Voronoi(r_km.cluster_centers_)
+#voronoi_plot_2d(vor)
+
+#Step size of the mesh. Decrease to increase the quality of the VQ.
+h = 500     # point in the mesh [x_min, x_max]x[y_min, y_max].
 
 # Plot the decision boundary. For that, we will assign a color to each
 x_min, x_max = reduced_data[:, 0].min() - 1, reduced_data[:, 0].max() + 1
@@ -44,7 +31,7 @@ y_min, y_max = reduced_data[:, 1].min() - 1, reduced_data[:, 1].max() + 1
 xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
 
 # Obtain labels for each point in mesh. Use last trained model.
-Z = kmeans.predict(np.c_[xx.ravel(), yy.ravel()])
+Z = km.predict(np.c_[xx.ravel(), yy.ravel()])
 
 # Put the result into a color plot
 Z = Z.reshape(xx.shape)
@@ -57,7 +44,7 @@ plt.imshow(Z, interpolation='nearest',
 
 plt.plot(reduced_data[:, 0], reduced_data[:, 1], 'k.', markersize=2)
 # Plot the centroids as a white X
-centroids = kmeans.cluster_centers_
+centroids = km.cluster_centers_
 plt.scatter(centroids[:, 0], centroids[:, 1],
             marker='x', s=169, linewidths=3,
             color='w', zorder=10)
@@ -68,4 +55,4 @@ plt.ylim(y_min, y_max)
 plt.xticks(())
 plt.yticks(())
 plt.show()
-"""
+
