4eb006c544994cfe505f971c4ee396dec0e95f1d
Steffany Bahamon <sbahamon1@gmail.com>
2016-10-12 16:41:22 -0500

    Update medicare_clustering.py

--- medicare_clustering.py
+++ medicare_clustering.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Oct 12 11:21:01 2016
+Created on Wed Oct 12 14:53:10 2016
 
-@author: sbv092
+@author: Steffany
 """
 import numpy as np
 from sklearn.cluster import KMeans
@@ -13,6 +13,8 @@ data = np.loadtxt("clean_providers.csv", delimiter = ",")
 #cluster
 km = KMeans(n_clusters = 6).fit(data)
 kmlabels = km.labels_
+kmcentroids = km.cluster_centers_
 
 silhouette_avg = silhouette_score(data, kmlabels, sample_size = 1000)
 print("The average silhouette_score is :", silhouette_avg)
+print(kmcentroids)
